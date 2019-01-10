a328a838ae89bdbaeec36942cadfd2d8a8a1c92f
Spened <lnoyaret@hotmail.fr>
2019-01-10 09:30:57 +0100

    Ajout de classe "ClassBDD" + "ClassMetier" + Ajout de réfrentiel pgsql

---
+++ ClassBDD/ClassBDD.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+//On inclue la librairie
+using Npgsql;
+using NpgsqlTypes;
+//Fin
+using System.Linq;
+using System.Data;
+using System.Text;
+
+
+namespace ClassBDD
+{
+    public class Connexion
+    {
+        string Conx = "Server=localhost;Port=6666;Database=postgres;User Id=openpg;Password=;";
+        NpgsqlCommand MyCmd = null;
+        NpgsqlConnection MyCnx = null;
+
+        public string ConnnexionBDD()
+        {
+            MyCnx = new NpgsqlConnection(Conx);
+            MyCnx.Open();
+            if(MyCnx.State == System.Data.ConnectionState.Open)
+            {
+                return "Connexion BDD Réussite !";
+            }
+            else
+            {
+                return "Connexion BDD Échoué !";
+            }
+        }        
+    }
+}
---
+++ ClassMetier/ClassMetier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassMetier
+{
+    public class Critere
+    {
+        private int id;
+        private string libel;
+        private int coef;
+
+        public void critere(int unId, string unLibel, int unCoef)
+        {
+            this.id = unId;
+            this.libel = unLibel;
+            this.coef = unCoef;
+        }
+    }
+}
