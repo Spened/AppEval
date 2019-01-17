f58ae16a75604400ed9bc67c54877f5cd33ec379
lysiak <david.lysiak99@gmail.com>
2019-01-17 12:02:08 +0100

    Modif design Connexion

--- AppEval/Connexion.Designer.cs
+++ AppEval/Connexion.Designer.cs
@@ -30,40 +30,32 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.lblConnexion = new System.Windows.Forms.Label();
             this.btnConnecter = new System.Windows.Forms.Button();
             this.lblIdentifiant = new System.Windows.Forms.Label();
             this.lblMDP = new System.Windows.Forms.Label();
+            this.grpConnexion = new System.Windows.Forms.GroupBox();
+            this.grpConnexion.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(358, 152);
+            this.textBox1.Location = new System.Drawing.Point(114, 45);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(358, 229);
+            this.textBox2.Location = new System.Drawing.Point(114, 122);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 1;
             // 
-            // lblConnexion
-            // 
-            this.lblConnexion.AutoSize = true;
-            this.lblConnexion.Location = new System.Drawing.Point(368, 95);
-            this.lblConnexion.Name = "lblConnexion";
-            this.lblConnexion.Size = new System.Drawing.Size(57, 13);
-            this.lblConnexion.TabIndex = 2;
-            this.lblConnexion.Text = "Connexion";
-            // 
             // btnConnecter
             // 
-            this.btnConnecter.Location = new System.Drawing.Point(358, 307);
+            this.btnConnecter.Location = new System.Drawing.Point(114, 182);
             this.btnConnecter.Name = "btnConnecter";
-            this.btnConnecter.Size = new System.Drawing.Size(91, 23);
+            this.btnConnecter.Size = new System.Drawing.Size(100, 23);
             this.btnConnecter.TabIndex = 3;
             this.btnConnecter.Text = "Se connecter";
             this.btnConnecter.UseVisualStyleBackColor = true;
@@ -71,7 +63,7 @@
             // lblIdentifiant
             // 
             this.lblIdentifiant.AutoSize = true;
-            this.lblIdentifiant.Location = new System.Drawing.Point(260, 155);
+            this.lblIdentifiant.Location = new System.Drawing.Point(16, 48);
             this.lblIdentifiant.Name = "lblIdentifiant";
             this.lblIdentifiant.Size = new System.Drawing.Size(53, 13);
             this.lblIdentifiant.TabIndex = 4;
@@ -80,27 +72,37 @@
             // lblMDP
             // 
             this.lblMDP.AutoSize = true;
-            this.lblMDP.Location = new System.Drawing.Point(260, 232);
+            this.lblMDP.Location = new System.Drawing.Point(16, 125);
             this.lblMDP.Name = "lblMDP";
             this.lblMDP.Size = new System.Drawing.Size(71, 13);
             this.lblMDP.TabIndex = 5;
             this.lblMDP.Text = "Mot de passe";
+            // 
+            // grpConnexion
+            // 
+            this.grpConnexion.Controls.Add(this.lblMDP);
+            this.grpConnexion.Controls.Add(this.lblIdentifiant);
+            this.grpConnexion.Controls.Add(this.btnConnecter);
+            this.grpConnexion.Controls.Add(this.textBox2);
+            this.grpConnexion.Controls.Add(this.textBox1);
+            this.grpConnexion.Location = new System.Drawing.Point(268, 109);
+            this.grpConnexion.Name = "grpConnexion";
+            this.grpConnexion.Size = new System.Drawing.Size(254, 246);
+            this.grpConnexion.TabIndex = 6;
+            this.grpConnexion.TabStop = false;
+            this.grpConnexion.Text = "Connexion";
             // 
             // Connexion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.lblMDP);
-            this.Controls.Add(this.lblIdentifiant);
-            this.Controls.Add(this.btnConnecter);
-            this.Controls.Add(this.lblConnexion);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.grpConnexion);
             this.Name = "Connexion";
             this.Text = "Connexion";
+            this.grpConnexion.ResumeLayout(false);
+            this.grpConnexion.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -108,9 +110,9 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label lblConnexion;
         private System.Windows.Forms.Button btnConnecter;
         private System.Windows.Forms.Label lblIdentifiant;
         private System.Windows.Forms.Label lblMDP;
+        private System.Windows.Forms.GroupBox grpConnexion;
     }
 }
