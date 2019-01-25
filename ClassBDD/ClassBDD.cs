9284983b67d2f3378c65fec5d958b026832f7bc5
PC <coquetpaul@outlook.fr>
2019-01-25 14:23:02 +0100

    null2

--- ClassBDD/ClassBDD.cs
+++ ClassBDD/ClassBDD.cs
@@ -20,24 +20,23 @@ namespace ClassBDD
         {
             string str = "";
             bool boolfound = false;
-            using (NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;Port=6666;Database=postgres;User Id=louis;Password=passwd"))
-            {
-                conn.Open();
+            conn().Open();
 
-                NpgsqlCommand cmd = new NpgsqlCommand("SELECT * FROM public.offre_d_emploi", conn);
-                NpgsqlDataReader dr = cmd.ExecuteReader();
-                if (dr.Read())
-                {
-                    boolfound = true;
-                    str = "connection established";
-                }
-                if (boolfound == false)
-                {
-                    str = "Data does not exist";
-                }
-                dr.Close();
-                return str;
+            NpgsqlCommand cmd = new NpgsqlCommand("SELECT * FROM public.offre_d_emploi", conn);
+            NpgsqlDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                boolfound = true;
+                str = "connection established";
             }
+            if (boolfound == false)
+            {
+                str = "Data does not exist";
+            }
+            dr.Close();
+            conn().Close();
+            return str;
+            
         }
 
         public NpgsqlConnection conn()
@@ -56,27 +55,26 @@ namespace ClassBDD
 
         public List<ClassMetier.OffreEmplois> GetOffreEmplois()
         {
-            using (NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;Port=6666;Database=postgres;User Id=louis;Password=passwd"))
+        
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            List<ClassMetier.OffreEmplois> lesOffresEmplois = new List<ClassMetier.OffreEmplois>();
+            conn().Open();
+            // Define a query
+            NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM public.offre_d_emploi", conn);
+            // Execute the query and obtain a result set
+            NpgsqlDataReader dr = command.ExecuteReader();
+            // Output rows
+            while (dr.Read())
             {
-                DataSet ds = new DataSet();
-                DataTable dt = new DataTable();
-                List<ClassMetier.OffreEmplois> lesOffresEmplois = new List<ClassMetier.OffreEmplois>();
-                conn.Open();
-                // Define a query
-                NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM public.offre_d_emploi", conn);
-                // Execute the query and obtain a result set
-                NpgsqlDataReader dr = command.ExecuteReader();
-                // Output rows
-                while (dr.Read())
-                {
-                    int id = int.Parse(dr[0].ToString());
-                    DateTime date = DateTime.Parse(dr[2].ToString());
-                    ClassMetier.OffreEmplois newOffre = new ClassMetier.OffreEmplois(id, dr[1].ToString(), date);
-                    lesOffresEmplois.Add(newOffre);
-                }
-                conn.Close();
-                return lesOffresEmplois;
+                int id = int.Parse(dr[0].ToString());
+                DateTime date = DateTime.Parse(dr[2].ToString());
+                ClassMetier.OffreEmplois newOffre = new ClassMetier.OffreEmplois(id, dr[1].ToString(), date);
+                lesOffresEmplois.Add(newOffre);
             }
+            conn().Close();
+            return lesOffresEmplois;
+            
         }
     }
 }
